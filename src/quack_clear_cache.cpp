25758abb244af870c2efe71857d876dbdfe057b5
Jose Davila-Ciullo <jose.davilaciullo@tinuiti.com>
2026-05-20 20:09:45 -0500

    Add targeted Quack cache refresh

--- src/quack_clear_cache.cpp
+++ src/quack_clear_cache.cpp
@@ -1,5 +1,7 @@
 #include "quack_clear_cache.hpp"
 
+#include "duckdb/common/exception.hpp"
+#include "duckdb/function/function_set.hpp"
 #include "duckdb/function/table_function.hpp"
 #include "duckdb/main/attached_database.hpp"
 #include "duckdb/main/client_context.hpp"
@@ -11,13 +13,24 @@ namespace duckdb {
 
 struct ClearCacheFunctionData : public TableFunctionData {
 	bool finished = false;
+	string catalog_name;
 };
 
-unique_ptr<FunctionData> ClearCacheBind(ClientContext &, TableFunctionBindInput &, vector<LogicalType> &return_types,
-                                        vector<string> &names) {
+unique_ptr<FunctionData> ClearCacheBind(ClientContext &, TableFunctionBindInput &input,
+                                        vector<LogicalType> &return_types, vector<string> &names) {
+	auto bind_data = make_uniq<ClearCacheFunctionData>();
+	if (!input.inputs.empty()) {
+		if (input.inputs[0].IsNull()) {
+			throw InvalidInputException("Catalog name cannot be NULL");
+		}
+		bind_data->catalog_name = input.inputs[0].GetValue<string>();
+		if (bind_data->catalog_name.empty()) {
+			throw InvalidInputException("Catalog name cannot be empty");
+		}
+	}
 	return_types.emplace_back(LogicalType::BOOLEAN);
 	names.emplace_back("Success");
-	return make_uniq<ClearCacheFunctionData>();
+	return std::move(bind_data);
 }
 
 void ClearQuackCaches(ClientContext &context) {
@@ -31,17 +44,38 @@ void ClearQuackCaches(ClientContext &context) {
 	}
 }
 
-void ClearCacheFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &) {
+void ClearQuackCache(ClientContext &context, const string &catalog_name) {
+	auto db = DatabaseManager::Get(context).GetDatabase(context, catalog_name);
+	if (!db) {
+		throw CatalogException("Failed to find attached database \"%s\"", catalog_name);
+	}
+	auto &catalog = db->GetCatalog();
+	if (catalog.GetCatalogType() != "quack") {
+		throw BinderException("Attached database \"%s\" does not refer to a Quack database", catalog_name);
+	}
+	catalog.Cast<QuackCatalog>().Refresh(context);
+}
+
+void ClearCacheFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
 	auto &data = data_p.bind_data->CastNoConst<ClearCacheFunctionData>();
 	if (data.finished) {
 		return;
 	}
-	ClearQuackCaches(context);
+	if (data.catalog_name.empty()) {
+		ClearQuackCaches(context);
+	} else {
+		ClearQuackCache(context, data.catalog_name);
+	}
+	output.SetValue(0, 0, Value::BOOLEAN(true));
+	output.SetCardinality(1);
 	data.finished = true;
 }
 
-TableFunction QuackClearCacheFunction::GetFunction() {
-	return TableFunction("quack_clear_cache", {}, ClearCacheFunction, ClearCacheBind);
+TableFunctionSet QuackClearCacheFunction::GetFunction() {
+	TableFunctionSet set("quack_clear_cache");
+	set.AddFunction(TableFunction("quack_clear_cache", {}, ClearCacheFunction, ClearCacheBind));
+	set.AddFunction(TableFunction("quack_clear_cache", {LogicalType::VARCHAR}, ClearCacheFunction, ClearCacheBind));
+	return set;
 }
 
 } // namespace duckdb
