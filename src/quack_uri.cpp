b7a94770efb40b0ea3d2ecd321192ac778b3a45e
Mytherin <mark.raasveldt@gmail.com>
2026-08-19 13:05:57 +0200

    Fix up quack for new DuckDB version

--- src/quack_uri.cpp
+++ src/quack_uri.cpp
@@ -86,13 +86,16 @@ static void QuackUriParser(const DataChunk &args, ExpressionState &, Vector &res
 
 // just for testing
 ScalarFunction QuackParseUriFunction::GetFunction() {
-	return ScalarFunction("quack_uri_parser", {/* uri */ LogicalType::VARCHAR, /* ssl */ LogicalType::BOOLEAN},
-	                      LogicalType::STRUCT({{"host", LogicalType::VARCHAR},
-	                                           {"port", LogicalType::USMALLINT},
-	                                           {"ipv6", LogicalType::BOOLEAN},
-	                                           {"ssl", LogicalType::BOOLEAN},
-	                                           {"url", LogicalType::VARCHAR}}),
-	                      QuackUriParser);
+	ScalarFunction function("quack_uri_parser", {/* uri */ LogicalType::VARCHAR, /* ssl */ LogicalType::BOOLEAN},
+	                        LogicalType::STRUCT({{"host", LogicalType::VARCHAR},
+	                                             {"port", LogicalType::USMALLINT},
+	                                             {"ipv6", LogicalType::BOOLEAN},
+	                                             {"ssl", LogicalType::BOOLEAN},
+	                                             {"url", LogicalType::VARCHAR}}),
+	                        QuackUriParser);
+	// parsing rejects malformed URIs at runtime, so constant folding must not treat a throw as internal
+	function.SetFallible();
+	return function;
 }
 
 } // namespace duckdb
